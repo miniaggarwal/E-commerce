a6826a3137ba197dce8b6233d6f2d99c80106cda
miniaggarwal <priyankaagarwal608@gmail.com>
2025-03-16 13:24:08 +0530

    Adding the encryption on password,compare password, JWT token genation and geneation of forgot password token to user.schema.js file

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const config = {
     PORT : process.env.PORT || 5000,
-    DB_URL : process.env.DB_URL || "mongodb://localhost:27017/E-Comm"
+    DB_URL : process.env.DB_URL || "mongodb://localhost:27017/E-Comm",
+    JWT_SECRET : process.env.JWT_SECRET || "yoursecretkey", 
+    JWT_EXPIRY : process.env.JWT_EXPIRY || "7d"
 }
 
 
